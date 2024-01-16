ecec7be2fe13c59a8a32a71fffc9b0dd449cca88
Maud Gautier <maud.j.gautier@gmail.com>
2024-01-15 17:29:39 -0700

    feat: improve display when launching multiple files

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,6 +1,11 @@
 for file_name in $@
 do
-  echo "-------------- RUNNING TESTS FOR FILE:" $file_name "-------------"
+  title="                    RUNNING TESTS FOR FILE: $file_name                   "
+  length=${#title}
+  echo -e "\n\n"
+  for i in $( seq 0 $length ); do echo -n '-'; done
+  echo -e "\n$title"
+  for i in $( seq 0 $length ); do echo -n '-'; done
   npm test $file_name
 done
 
